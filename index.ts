967f79c6d1c58edcae49e7c5cb1e38d9a5af87d2
Mark Yan <t34@qq.com>
2026-03-15 20:50:41 +0800

    Add seller assistant plugin load log

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ import { registerSellerTools } from "./src/tools.js"
 
 /** Registers the seller assistant plugin tools */
 export default function register(api: OpenClawPluginApi) {
+  api.logger.info(
+    `[seller-assistant] plugin loaded id=${api.id} version=${api.version ?? "unknown"}`,
+  )
   const pluginConfig = toPluginConfig(api)
   registerSellerTools(api, pluginConfig)
 }
